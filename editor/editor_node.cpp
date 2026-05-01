46e6e17aa8748bd7b33f759c55e50d1b57e0e04d
Star Court <nnerror811@gmail.com>
2026-05-01 16:02:35 +0530

    Change the warning logic for display scale

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -1043,13 +1043,14 @@ void EditorNode::_notification(int p_what) {
 			}
 			
 #ifdef ANDROID_ENABLED
-			float scale = EditorScale::get_scale();
-            float screen_width = DisplayServer::get_singleton()->screen_get_size().x;
-            if (scale * 1024.0 > screen_width) {
-                EditorToaster::get_singleton()->popup_str(TTR("The selected display scale may cause the editor UI to overflow the screen on this device. Consider using a lower scale."), EditorToaster::SEVERITY_WARNING);
-            }
+			Window *w = get_window();
+			if (w) {
+				float screen_width = w->get_size().x;
+				if (EditorScale::get_scale() * 1024.0 > screen_width) {
+					EditorToaster::get_singleton()->popup_str(TTR("The selected display scale may cause the editor UI to overflow the screen on this device. Consider using a lower scale."), EditorToaster::SEVERITY_WARNING);
+				}
+			}
 #endif
-
 			/* DO NOT LOAD SCENES HERE, WAIT FOR FILE SCANNING AND REIMPORT TO COMPLETE */
 		} break;
 
